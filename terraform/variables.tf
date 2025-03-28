dbb06a53b943942f7d2766d62c617f9035fba866
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-03-28 20:22:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
-#variable "aws_region" {
- # description = "AWS region to deploy resources"
- # type        = string
- # default     = "us-east-1"
-#}
+ variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
 #variable "environment" {
  # description = "Deployment environment (dev, staging, prod)"
