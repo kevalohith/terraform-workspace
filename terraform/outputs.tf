1fd02f43d41a9af7767f4b3e27b0d19bbcb38fda
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-03-28 17:17:23 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,3 @@ output "public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_instance.web.vpc_security_group_ids
-}
-
