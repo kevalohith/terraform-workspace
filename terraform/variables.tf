c652299e31dcdf363e268c0ac7a0f5b2bc724d14
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-03-28 20:20:41 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-}
+#variable "aws_region" {
+ # description = "AWS region to deploy resources"
+ # type        = string
+ # default     = "us-east-1"
+#}
 
-variable "environment" {
-  description = "Deployment environment (dev, staging, prod)"
-  type        = string
-  default     = "dev"
-}
+#variable "environment" {
+ # description = "Deployment environment (dev, staging, prod)"
+ # type        = string
+ # default     = "dev"
+#}
 
 
-variable "instance_type" {
-  description = "EC2 instance type based on environment"
-  type        = map(string)
-  default = {
-    dev     = "t2.micro"
-    staging = "t3.small"
-    prod    = "t3.medium"
-  }
-}
+#variable "instance_type" {
+  #description = "EC2 instance type based on environment"
+  #type        = map(string)
+  #default = {
+    #dev     = "t2.micro"
+    #staging = "t3.small"
+    #prod    = "t3.medium"
+  #}
+#}
 
