ba047433b5b1c62caf73313c6cf1d7925fe15110
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-03-28 20:15:12 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket         = "kevloh-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
-}
-
 provider "aws" {
   region = var.aws_region
 }
