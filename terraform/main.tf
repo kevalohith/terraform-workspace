7411cdd88be59d5023ca28207bf723f3c84c7cd9
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-03-28 17:16:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,17 @@ locals {
 
 # Create an S3 Bucket with a unique name
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${local.workspace_name}-terraform-bucket"
+  bucket = "${local.workspace_name}-kevloh-bucket"
   acl    = "private"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (Change as per region)
+  ami           = "ami-071226ecf16aa7d96"  # Amazon Linux 2 AMI (Change as per region)
   instance_type = lookup(var.instance_type, local.workspace_name, "t2.micro")
 
   tags = {
-    Name = "${local.workspace_name}-ec2"
+    Name = "${local.workspace_name}-kl"
   }
 }
 
